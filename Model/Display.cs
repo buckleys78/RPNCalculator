a0f5a8bdcef93bf8b623ea694721307aa9f3ce71
Steve Buckley <buckleys78@gmail.com>
2015-11-01 11:03:03 -0800

    made Display class part of Model namespace.

--- Model/Display.cs
+++ Model/Display.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RPNCalculator {
+namespace RPNCalculator.Model {
     class Display {
         public string Buffer { get; set; }
 
